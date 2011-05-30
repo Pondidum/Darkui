205ebb8a11a9064351b21c4274d7f06012eda0a1
Pondidum <pondidum@stormbase.net>
2011-05-30 15:01:39 +0100

    formatting

--- modules/Stats/Durability.lua
+++ modules/Stats/Durability.lua
@@ -4,7 +4,6 @@ if S.stats.enable == false or S.stats.durability < 0 then return end
 
 local frame = S.stats.panel:Add("Durability", S.stats.durability)
 
-
 local slots = {
 	[1] = {1, "Head", 1000},
 	[2] = {3, "Shoulder", 1000},
@@ -25,24 +24,29 @@ local function GetData()
 	local current, max
 	
 	for i = 1, 11 do
+	
 		if GetInventoryItemLink("player", slots[i][1]) ~= nil then
+			
 			current, max = GetInventoryItemDurability(slots[i][1])
+			
 			if current then 
-				slots[i][3] = current/max
+				slots[i][3] = current / max
 				total = total + 1
 			end
+			
 		end
+		
 	end
 	table.sort(slots, function(a, b) return a[3] < b[3] end)
 	
 	if total > 0 then
-		frame.text:SetText(floor(slots[1][3]*100).."% Armor")
+		frame.text:SetText(floor(slots[1][3] * 100).."% Armor")
 	else
 		frame.text:SetText("100% Armor")
 	end
 	
 	total = 0
-		
+	
 end
 
 E:Register("UPDATE_INVENTORY_DURABILITY", GetData)
