3391a6e6278edc61d2ce94e037d6cd8e1e365cf1
Pondidum <pondidum@stormbase.net>
2011-06-15 16:20:54 +0100

    added slash commands: sPrint, rl and frame

---
+++ core/commands.lua
@@ -0,0 +1,54 @@
+local D, S, E = unpack(select(2, ...))
+
+SLASH_RELOADUI1 = "/rl"
+SlashCmdList.RELOADUI = ReloadUI
+
+SLASH_FRAME1 = "/frame"
+SlashCmdList["FRAME"] = function(arg)
+	if arg ~= "" then
+		arg = _G[arg]
+	else
+		arg = GetMouseFocus()
+	end
+	if arg ~= nil and arg:GetName() ~= nil then
+		local point, relativeTo, relativePoint, xOfs, yOfs = arg:GetPoint()
+		ChatFrame1:AddMessage("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		ChatFrame1:AddMessage("Name: |cffFFD100"..arg:GetName())
+		if arg:GetParent() then
+			ChatFrame1:AddMessage("Parent: |cffFFD100"..arg:GetParent():GetName())
+		end
+ 
+		ChatFrame1:AddMessage("Width: |cffFFD100"..format("%.2f",arg:GetWidth()))
+		ChatFrame1:AddMessage("Height: |cffFFD100"..format("%.2f",arg:GetHeight()))
+		ChatFrame1:AddMessage("Strata: |cffFFD100"..arg:GetFrameStrata())
+		ChatFrame1:AddMessage("Level: |cffFFD100"..arg:GetFrameLevel())
+ 
+		if xOfs then
+			ChatFrame1:AddMessage("X: |cffFFD100"..format("%.2f",xOfs))
+		end
+		if yOfs then
+			ChatFrame1:AddMessage("Y: |cffFFD100"..format("%.2f",yOfs))
+		end
+		if relativeTo then
+			ChatFrame1:AddMessage("Point: |cffFFD100"..point.."|r anchored to "..relativeTo:GetName().."'s |cffFFD100"..relativePoint)
+		end
+		ChatFrame1:AddMessage("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	elseif arg == nil then
+		ChatFrame1:AddMessage("Invalid frame name")
+	else
+		ChatFrame1:AddMessage("Could not find frame info")
+	end
+end
+
+SLASH_DARKPRINT1 = "/sprint"
+SlashCmdList["DARKPRINT"] = function (args)
+
+	local result, message = pcall(loadstring(args))
+	
+	if result then
+		print(message)
+	else
+		print("Fail: " .. message)
+	end
+	
+end
