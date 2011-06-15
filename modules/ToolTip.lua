9df9e593465acaee7e1d7e32e071c49d36d5d6c5
Pondidum <pondidum@stormbase.net>
2011-06-15 14:53:50 +0100

    added custom tooltip

---
+++ modules/ToolTip.lua
@@ -0,0 +1,417 @@
+local D, S, E = unpack(select(2, ...))
+-- credits : Aezay (TipTac) and Caellian for some parts of code.
+
+local DarkuiToolTip = CreateFrame("Frame", "DarkuiToolTip", UIParent)
+local _G = getfenv(0)
+
+local GameTooltip, GameTooltipStatusBar = _G["GameTooltip"], _G["GameTooltipStatusBar"]
+local gsub, find, format = string.gsub, string.find, string.format
+
+local Tooltips = {GameTooltip,ShoppingTooltip1,ShoppingTooltip2,ShoppingTooltip3,WorldMapTooltip}
+local ItemRefTooltip = ItemRefTooltip
+
+local linkTypes = {item = true, enchant = true, spell = true, quest = true, unit = true, talent = true, achievement = true, glyph = true}
+
+local classification = {
+	worldboss = "|cffAF5050Boss|r",
+	rareelite = "|cffAF5050+ Rare|r",
+	elite = "|cffAF5050+|r",
+	rare = "|cffAF5050Rare|r",
+}
+
+local NeedBackdropBorderRefresh = true
+
+local anchor = CreateFrame("Frame", "DarkuiTooltipAnchor", UIParent)
+anchor:SetSize(200, 1)
+anchor:SetFrameStrata("TOOLTIP")
+anchor:SetFrameLevel(20)
+anchor:SetClampedToScreen(true)
+anchor:SetAlpha(0)
+anchor:SetPoint("BOTTOMLEFT", DarkuiActionBarBackground, "BOTTOMRIGHT", 5, 0)
+anchor:SetBackdropBorderColor(1, 0, 0, 1)
+anchor:SetMovable(true)
+anchor.text = D.CreateFontString(anchor, S.fonts.normal, 12)
+anchor.text:SetPoint("CENTER")
+anchor.text:SetText("Move Tooltip")
+
+-- Update Tukui Tooltip Position on some specifics Tooltip
+-- Also used because on Eyefinity, SetClampedToScreen doesn't work on left and right side of screen #1
+local function UpdateTooltip(self)
+	local owner = self:GetOwner()
+	if not owner then return end	
+	local name = owner:GetName()
+	
+	-- fix X-offset or Y-offset
+	local x = 5
+	
+	-- mouseover
+	if self:GetAnchorType() == "ANCHOR_CURSOR" then
+		
+		-- h4x for world object tooltip border showing last border color or showing background sometime ~blue :x
+		if NeedBackdropBorderRefresh then
+			NeedBackdropBorderRefresh = false			
+			self:SetBackdropColor(unpack(S.colors.default.background))
+			self:SetBackdropBorderColor(unpack(S.colors.default.border))
+		end
+		
+	elseif self:GetAnchorType() == "ANCHOR_NONE" and InCombatLockdown() and S.tooltip.hideincombat == true then
+		self:Hide()
+	end
+	
+	if name and (TukuiPlayerBuffs or TukuiPlayerDebuffs) then
+		if (TukuiPlayerBuffs:GetPoint():match("LEFT") or TukuiPlayerDebuffs:GetPoint():match("LEFT")) and (name:match("TukuiPlayerBuffs") or name:match("TukuiPlayerDebuffs")) then
+			self:SetAnchorType("ANCHOR_BOTTOMRIGHT", x, -x)
+		end
+	end
+		
+	if (owner == MiniMapBattlefieldFrame or owner == MiniMapMailFrame) and TukuiMinimap then
+		if TukuiMinimap:GetPoint():match("LEFT") then 
+			self:SetAnchorType("ANCHOR_TOPRIGHT", x, -x)
+		end
+	end
+	
+	if self:GetAnchorType() == "ANCHOR_NONE" and DarkuiTooltipAnchor then
+	
+		local point = DarkuiTooltipAnchor:GetPoint()
+	
+		if point == "TOPLEFT" then
+			self:ClearAllPoints()
+			self:SetPoint("TOPLEFT", DarkuiTooltipAnchor, "BOTTOMLEFT", 0, -x)			
+		elseif point == "TOP" then
+			self:ClearAllPoints()
+			self:SetPoint("TOP", DarkuiTooltipAnchor, "BOTTOM", 0, -x)			
+		elseif point == "TOPRIGHT" then
+			self:ClearAllPoints()
+			self:SetPoint("TOPRIGHT", DarkuiTooltipAnchor, "BOTTOMRIGHT", 0, -x)			
+		elseif point == "BOTTOMLEFT" or point == "LEFT" then
+			self:ClearAllPoints()
+			self:SetPoint("BOTTOMLEFT", DarkuiTooltipAnchor, "TOPLEFT", 0, x)		
+		elseif point == "BOTTOMRIGHT" or point == "RIGHT" then
+			if TukuiBags and TukuiBags:IsShown() then
+				self:ClearAllPoints()
+				self:SetPoint("BOTTOMRIGHT", TukuiBags, "TOPRIGHT", 0, x)			
+			else
+				self:ClearAllPoints()
+				self:SetPoint("BOTTOMRIGHT", DarkuiTooltipAnchor, "TOPRIGHT", 0, x)
+			end
+		else
+			self:ClearAllPoints()
+			self:SetPoint("BOTTOM", DarkuiTooltipAnchor, "TOP", 0, x)		
+		end
+		
+	end
+	
+end
+
+hooksecurefunc("GameTooltip_SetDefaultAnchor", function(self, parent)
+	self:SetOwner(parent, "ANCHOR_NONE")
+end)
+
+GameTooltip:HookScript("OnUpdate", function(self, ...) UpdateTooltip(self) end)
+
+local function Hex(color)
+	return string.format('|cff%02x%02x%02x', color.r * 255, color.g * 255, color.b * 255)
+end
+
+local function GetColor(unit)
+	if(UnitIsPlayer(unit) and not UnitHasVehicleUI(unit)) then
+		local _, class = UnitClass(unit)
+		local color = RAID_CLASS_COLORS[class]
+		if not color then return end -- sometime unit too far away return nil for color :(
+		local r,g,b = color.r, color.g, color.b
+		return Hex(color), r, g, b	
+	else
+		local color = FACTION_BAR_COLORS[UnitReaction(unit, "player")]
+		if not color then return end -- sometime unit too far away return nil for color :(
+		local r,g,b = color.r, color.g, color.b		
+		return Hex(color), r, g, b		
+	end
+end
+
+-- function to short-display HP value on StatusBar
+local function ShortValue(value)
+	if value >= 1e7 then
+		return ('%.1fm'):format(value / 1e6):gsub('%.?0+([km])$', '%1')
+	elseif value >= 1e6 then
+		return ('%.2fm'):format(value / 1e6):gsub('%.?0+([km])$', '%1')
+	elseif value >= 1e5 then
+		return ('%.0fk'):format(value / 1e3)
+	elseif value >= 1e3 then
+		return ('%.1fk'):format(value / 1e3):gsub('%.?0+([km])$', '%1')
+	else
+		return value
+	end
+end
+
+-- update HP value on status bar
+GameTooltipStatusBar:SetScript("OnValueChanged", function(self, value)
+	if not value then
+		return
+	end
+	local min, max = self:GetMinMaxValues()
+	
+	if (value < min) or (value > max) then
+		return
+	end
+	local _, unit = GameTooltip:GetUnit()
+	
+	-- fix target of target returning nil
+	if (not unit) then
+		local GMF = GetMouseFocus()
+		unit = GMF and GMF:GetAttribute("unit")
+	end
+
+	if not self.text then
+		self.text = self:CreateFontString(nil, "OVERLAY")
+		local position = DarkuiTooltipAnchor:GetPoint()
+		if position:match("TOP") then
+			self.text:SetPoint("CENTER", GameTooltipStatusBar, 0, -6)
+		else
+			self.text:SetPoint("CENTER", GameTooltipStatusBar, 0, 6)
+		end
+		
+		self.text:SetFont(S.fonts.normal, 12, "THINOUTLINE")
+		self.text:Show()
+		if unit then
+			min, max = UnitHealth(unit), UnitHealthMax(unit)
+			local hp = ShortValue(min).." / "..ShortValue(max)
+			if UnitIsGhost(unit) then
+				self.text:SetText("Ghost")
+			elseif min == 0 or UnitIsDead(unit) or UnitIsGhost(unit) then
+				self.text:SetText("Dead")
+			else
+				self.text:SetText(hp)
+			end
+		end
+	else
+		if unit then
+			min, max = UnitHealth(unit), UnitHealthMax(unit)
+			self.text:Show()
+			local hp = ShortValue(min).." / "..ShortValue(max)
+			if UnitIsGhost(unit) then
+				self.text:SetText("Ghost")
+			elseif min == 0 or UnitIsDead(unit) or UnitIsGhost(unit) then
+				self.text:SetText("Dead")
+			else
+				self.text:SetText(hp)
+			end
+		else
+			self.text:Hide()
+		end
+	end
+end)
+
+local healthBar = GameTooltipStatusBar
+healthBar:ClearAllPoints()
+healthBar:SetHeight(6)
+healthBar:SetPoint("BOTTOMLEFT", healthBar:GetParent(), "TOPLEFT", 0, 5)
+healthBar:SetPoint("BOTTOMRIGHT", healthBar:GetParent(), "TOPRIGHT", 0, 5)
+healthBar:SetStatusBarTexture(S.textures.normal)
+
+local healthBarBG = CreateFrame("Frame", "StatusBarBG", healthBar)
+healthBarBG:SetFrameLevel(healthBar:GetFrameLevel() - 1)
+healthBarBG:SetAllPoints(healthBar)
+D.CreateBackground(healthBarBG)
+D.CreateShadow(healthBarBG)
+
+GameTooltip:HookScript("OnTooltipSetUnit", function(self)
+	local lines = self:NumLines()
+	local GMF = GetMouseFocus()
+	local unit = (select(2, self:GetUnit())) or (GMF and GMF:GetAttribute("unit"))
+	
+	-- A mage's mirror images sometimes doesn't return a unit, this would fix it
+	if (not unit) and (UnitExists("mouseover")) then
+		unit = "mouseover"
+	end
+	
+	-- Sometimes when you move your mouse quicky over units in the worldframe, we can get here without a unit
+	if not unit then self:Hide() return end
+	
+	-- A "mouseover" unit is better to have as we can then safely say the tip should no longer show when it becomes invalid.
+	if (UnitIsUnit(unit,"mouseover")) then
+		unit = "mouseover"
+	end
+
+	local race = UnitRace(unit)
+	local class = UnitClass(unit)
+	local level = UnitLevel(unit)
+	local guild = GetGuildInfo(unit)
+	local name, realm = UnitName(unit)
+	local crtype = UnitCreatureType(unit)
+	local classif = UnitClassification(unit)
+	local title = UnitPVPName(unit)
+	local r, g, b = GetQuestDifficultyColor(level).r, GetQuestDifficultyColor(level).g, GetQuestDifficultyColor(level).b
+
+	local color = GetColor(unit)	
+	if not color then color = "|CFFFFFFFF" end -- just safe mode for when GetColor(unit) return nil for unit too far away
+
+	_G["GameTooltipTextLeft1"]:SetFormattedText("%s%s%s", color, title or name, realm and realm ~= "" and " - "..realm.."|r" or "|r")
+
+	if(UnitIsPlayer(unit)) then
+		if UnitIsAFK(unit) then
+			self:AppendText((" %s"):format(CHAT_FLAG_AFK))
+		elseif UnitIsDND(unit) then 
+			self:AppendText((" %s"):format(CHAT_FLAG_DND))
+		end
+
+		local offset = 2
+		if guild then
+			_G["GameTooltipTextLeft2"]:SetFormattedText("%s", IsInGuild() and GetGuildInfo("player") == guild and "|cff0090ff"..guild.."|r" or "|cff00ff10"..guild.."|r")
+			offset = offset + 1
+		end
+
+		for i= offset, lines do
+			if(_G["GameTooltipTextLeft"..i]:GetText():find("^"..LEVEL)) then
+				_G["GameTooltipTextLeft"..i]:SetFormattedText("|cff%02x%02x%02x%s|r %s %s%s", r*255, g*255, b*255, level > 0 and level or "??", race, color, class.."|r")
+				break
+			end
+		end
+	else
+		for i = 2, lines do
+			if((_G["GameTooltipTextLeft"..i]:GetText():find("^"..LEVEL)) or (crtype and _G["GameTooltipTextLeft"..i]:GetText():find("^"..crtype))) then
+				if level == -1 and classif == "elite" then classif = "worldboss" end
+				_G["GameTooltipTextLeft"..i]:SetFormattedText("|cff%02x%02x%02x%s|r%s %s", r*255, g*255, b*255, classif ~= "worldboss" and level ~= 0 and level or "", classification[classif] or "", crtype or "")
+				break
+			end
+		end
+	end
+
+	local pvpLine
+	for i = 1, lines do
+		local text = _G["GameTooltipTextLeft"..i]:GetText()
+		if text and text == PVP_ENABLED then
+			pvpLine = _G["GameTooltipTextLeft"..i]
+			pvpLine:SetText()
+			break
+		end
+	end
+
+	-- ToT line
+	if UnitExists(unit.."target") and unit~="player" then
+		local hex, r, g, b = GetColor(unit.."target")
+		if not r and not g and not b then r, g, b = 1, 1, 1 end
+		GameTooltip:AddLine(UnitName(unit.."target"), r, g, b)
+	end
+	
+	-- Sometimes this wasn't getting reset, the fact a cleanup isn't performed at this point, now that it was moved to "OnTooltipCleared" is very bad, so this is a fix
+	self.fadeOut = nil
+end)
+
+local BorderColor = function(self)
+	local GMF = GetMouseFocus()
+	local unit = (select(2, self:GetUnit())) or (GMF and GMF:GetAttribute("unit"))
+		
+	local reaction = unit and UnitReaction(unit, "player")
+	local player = unit and UnitIsPlayer(unit)
+	local tapped = unit and UnitIsTapped(unit)
+	local tappedbyme = unit and UnitIsTappedByPlayer(unit)
+	local connected = unit and UnitIsConnected(unit)
+	local dead = unit and UnitIsDead(unit)
+
+	local br, bg, bb = unpack(S.colors.default.background)
+	self.bg:SetBackdropColor(br, bg, bb, 0.8)
+	self.shadow:SetBackdropBorderColor(unpack(S.colors.default.border))
+	
+	if player then
+		
+		local class = select(2, UnitClass(unit))
+		local r, g, b = unpack(S.colors.class[class])
+		healthBar:SetStatusBarColor(r, g, b)
+		
+	elseif reaction then
+		
+		local r, g, b = unpack(S.colors.reaction[reaction])
+		healthBar:SetStatusBarColor(r, g, b)
+		
+	else
+		
+		local _, link = self:GetItem()
+		local quality = link and select(3, GetItemInfo(link))
+		
+		if quality and quality >= 2 then
+			local r, g, b = GetItemQualityColor(quality)
+			self.shadow:SetBackdropBorderColor(r, g, b)
+		else
+			healthBar:SetStatusBarColor(unpack(S.colors.default.background))
+		end
+		
+	end
+	
+	-- need this
+	NeedBackdropBorderRefresh = true
+end
+
+local SetStyle = function(self)
+	self:SetBackdrop(nil)
+	D.CreateBackground(self)
+	D.CreateShadow(self)
+	BorderColor(self)
+end
+
+local function EnteringWorldEventHandler(self)
+	
+	print("entering")
+	for _, tt in pairs(Tooltips) do
+		tt:HookScript("OnShow", SetStyle)
+	end
+	
+	ItemRefTooltip:HookScript("OnTooltipSetItem", SetStyle)
+	ItemRefTooltip:HookScript("OnShow", SetStyle)	
+	FriendsTooltip:SetTemplate("Default")
+		
+	E:Unregister("PLAYER_ENTERING_WORLD", "DarkuiTooltipEnteringWorld")
+	
+	-- move health status bar if anchor is found at top
+	local position = DarkuiTooltipAnchor:GetPoint()
+	if position:match("TOP") then
+		healthBar:ClearAllPoints()
+		healthBar:SetPoint("TOPLEFT", healthBar:GetParent(), "BOTTOMLEFT", 0, -5)
+		healthBar:SetPoint("TOPRIGHT", healthBar:GetParent(), "BOTTOMRIGHT", 0, -5)
+	end
+	
+end
+
+E:Register("PLAYER_ENTERING_WORLD", EnteringWorldEventHandler, "DarkuiTooltipEnteringWorld")
+
+hooksecurefunc(GameTooltip, "SetUnitBuff", function(self,...)
+	local id = select(11,UnitBuff(...))
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self,...)
+	local id = select(11,UnitDebuff(...))
+	if id then
+		self:AddDoubleLine("SpellID:",id)
+		self:Show()
+	end
+end)
+
+hooksecurefunc(GameTooltip, "SetUnitAura", function(self,...)
+	local name, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, shouldConsolidate, spellId  = UnitAura(...)
+	if spellId then
+		self:AddDoubleLine("SpellID",spellId)
+	end
+	if unitCaster then
+
+		local src = GetUnitName(unitCaster, true)
+		if unitCaster == "pet" or unitCaster == "vehicle" then
+			src = format("%s (%s)", src, GetUnitName("player", true))
+		else
+			local partypet = unitCaster:match("^partypet(%d+)$")
+			local raidpet = unitCaster:match("^raidpet(%d+)$")
+			if partypet then
+				src = format("%s (%s)", src, GetUnitName("party"..partypet, true))
+			elseif raidpet then
+				src = format("%s (%s)", src, GetUnitName("raid"..raidpet, true))
+			end
+		end
+
+		self:AddDoubleLine("Caster",src)
+	end
+	if spellId or unitCaster then
+		self:Show()
+	end
+end)
