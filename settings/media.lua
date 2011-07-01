8ea29b4b50f1700dc6725521069f8ac8446dc4a2
Pondidum <pondidum@stormbase.net>
2011-07-01 17:39:03 +0100

    split colors and media into seperate files

---
+++ settings/media.lua
@@ -0,0 +1,19 @@
+local D, S, E = unpack(select(2, ...))
+
+S["textures"] = {
+	["shadow"] = [[Interface\AddOns\Darkui\media\textures\glowTex]],
+	["normal"] = [[Interface\AddOns\Darkui\media\textures\normTex]],
+	["blank"] = [[Interface\AddOns\Darkui\media\textures\blank]],
+	["raidicons"] = [[Interface\AddOns\Darkui\media\textures\raidicons.blp]],
+	["buttonhover"] = [[Interface\AddOns\Darkui\medias\textures\button_hover]],
+}
+
+S["fonts"] = {
+	["unitframe"] = [[Interface\AddOns\Darkui\media\fonts\BigNoodleTitling.ttf]],
+	["normal"] = [[Interface\AddOns\Darkui\media\fonts\PT-Sans-Narrow.ttf]],
+	["combat"] = [[Interface\AddOns\Darkui\media\fonts\Ultima_Campagnoli.ttf]],
+	["default"] = {
+		["size"] = 12,
+		["style"] = "",
+	},
+}
