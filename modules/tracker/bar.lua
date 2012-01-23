b43485ef2750b672655c3157b3f614015cd98d1b
= <=>
2012-01-23 18:04:23 +0000

    more bug fixes

--- modules/tracker/bar.lua
+++ modules/tracker/bar.lua
@@ -11,7 +11,7 @@ function T.CreateBar(name, setup)
 
 	if setup.sizemode == "FIXED" then
 
-		container:SetSize(unpack(setup.containersize))
+		container:SetSize(unpack(setup.size))
 
 	elseif setup.sizemode == "TOPDOWN" or setup.sizemode == "BOTTOMUP" then
 
@@ -29,27 +29,43 @@ function T.CreateBar(name, setup)
 
 	container.UpdateDisplay = function(self)
 		
-		local collection = self.Data
-		local displayed = 0 
-
 		local function GetAnchors(mode, icon, index)
 			
 			if mode == "TOPDOWN" then
-				return "TOP", "TOP", 0, index * (icon:GetHeight() + 2)
+
+				return "TOP", "TOP", 0, index * (icon:GetHeight() + 4)
 
 			elseif mode == "BOTTOMUP" then
-				return "BOTTOM", "BOTTOM", 0, index * (icon:GetHeight() + 2)
+
+				return "BOTTOM", "BOTTOM", 0, index * (icon:GetHeight() + 4)
 
 			elseif mode == "LEFTRIGHT" then
-				return "LEFT", "LEFT", (index * (icon:GetWidth() + 2)), 0
+
+				return "LEFT", "LEFT", index * (icon:GetWidth() + 4), 0
 
 			elseif mode == "RIGHTLEFT" then
-				return "RIGHT", "RIGHT", index * (icon:GetWidth() + 2), 0 
+
+				return "RIGHT", "RIGHT", index * (icon:GetWidth() + 4), 0 
+
 			end
 				
 		end
+
+		local function CalculateFrameSize(containerSize)
+
+			if containerSize[1] < containerSize[2] then
+				return containerSize[1], containerSize[1]
+			else
+				return containerSize[2], containerSize[2]
+			end
+
+		end
+
 		
 		local current
+		local collection = self.Data
+		local displayed = 0 
+
 		for _, current in pairs(collection) do
 						
 			if current.display then
@@ -57,18 +73,18 @@ function T.CreateBar(name, setup)
 				local icon = self.Cache[current.id]
 
 				if icon == nil then
-					icon = T.CreateIcon(self, self:GetName() .. current.id, setup.location, setup.size)
+					icon = T.CreateIcon(self, self:GetName() .. current.id, setup.location, {CalculateFrameSize(setup.size)})
 					self.Cache[current.id] = icon
 				end
 
 				icon:UpdateIcon(current.texture)
 				icon:UpdateCooldown(current.expiry)
 
-				local anchor, relAnchor, x, y = GetAnchors(self.setup.mode, icon, displayed) 
+				local anchor, relAnchor, x, y = GetAnchors(setup.flowdirection, icon, displayed) 
 				icon:ClearAllPoints()
-				icon:SetPoint(anchor, self, "LEFT", x, y) 
+				icon:SetPoint(anchor, self, relAnchor, x, y) 
 				icon:Show()
-				
+
 				displayed = displayed + 1 
 			else
 				
