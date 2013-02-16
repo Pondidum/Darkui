1442b83814d4e0426751b6fdb6988e2c0d4c364e
Andy <andy@stormbase.net>
2013-02-16 14:00:34 +0000

    layout: update spacing

--- settings/layouts.lua
+++ settings/layouts.lua
@@ -52,52 +52,52 @@ S.unitframes.layouts["default"] = {
 	},
 }
 
---The healer config.  Raid panel appears centre screen, above actionbars, and is larger.  also shows hots etc on units.
+
 S.unitframes.layouts["healer"] = {
-	["floatingcastbars"] = false,
+	["floatingcastbars"] = true,
 	["buffwatch"] = true,
 	["player"] = {
-		["point"] = {"TOPRIGHT", "oUF_DarkuiRaid", "TOPLEFT", -35, 0}, 
-		["size"] = {240, 18},
+		["point"] = {"BOTTOM", "DarkuiActionBarBackground", "TOP", 0, 80},
+		["size"] = {220, 18},
 	},
 	["pet"] = {
-		["point"] = {"BOTTOMRIGHT", "oUF_DarkuiRaid", "BOTTOMLEFT", -35, 0}, 
+		["point"] = {"RIGHT", "oUF_DarkuiPlayer", "LEFT", -25, 0},
 		["size"] = {132, 18},
 	},
 	["target"] = {
-		["point"] = {"TOPLEFT", "oUF_DarkuiRaid", "TOPRIGHT", 35, 0}, 
-		["size"] = {240, 18},
+		["point"] = {"LEFT", "oUF_DarkuiPlayer", "RIGHT", 25, 100},
+		["size"] = {220, 18},
 	},
 	["focus"] = {
-		["point"] = {"BOTTOMLEFT", "oUF_DarkuiRaid", "BOTTOMRIGHT", 35, 0}, 
-		["size"] = {240, 18},
+		["point"] = {"RIGHT", "oUF_DarkuiPlayer", "LEFT", -25, 100},
+		["size"] = {220, 18},
 	},
 	["targettarget"] = {
-		["point"] = {"LEFT", "oUF_DarkuiTarget", "RIGHT", 25, 0}, 
+		["point"] = {"TOPRIGHT", "oUF_DarkuiTarget", "BOTTOMRIGHT", 0, -25},
 		["size"] = {132, 18},
 	},
 	["focustarget"] = {
-		["point"] = {"LEFT", "oUF_DarkuiFocus", "RIGHT", 25, 0}, 
+		["point"] = {"TOPLEFT", "oUF_DarkuiFocus", "BOTTOMLEFT", 0, -25},
 		["size"] = {132, 18},
 	},
 	["boss"] = {
-		["point"] = {"BOTTOMRIGHT", "DarkuiBar5", "BOTTOMLEFT", -50, -15}, 
+		["point"] = {"BOTTOMRIGHT", "DarkuiBar5", "BOTTOMLEFT", -50, -15},
 		["size"] = {132, 18},
 	},
-	["raidheader"] = {	--note raidheader has is position set, and not its size
-		["point"] = {"BOTTOM", "DarkuiFrame", "BOTTOM", 0, 150}, 
-		["size"] = {},
+	["raidheader"] = {		--note raidheader has is position set, and not its size
+		["point"] = {"BOTTOM", "oUF_DarkuiPlayer", "BOTTOM",  0, -30}, 
 	},
 	["raidgroup"] = {
-		anchor = "RIGHT",
-		xoffset = -5,
+		anchor = "LEFT",
+		xoffset = 5,
 		yoffset = 0,
 	},
-	["raidunit"] = {		--note raid has is size set, and not its position
-		["point"] = {"BOTTOMLEFT", "", "TOPLEFT", 0, 5},
-		["size"] = {80 , 36},
+	["raidunit"] = {			--note raid has is size set, and not its position
+		["point"] = {"LEFT", "", "RIGHT", 0, 0},
+		["size"] = {60 , 26},
 	},
 }
+
 
 S.unitframes.layouts["hybrid"] = {
 	["floatingcastbars"] = true,
